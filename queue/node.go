539109dcfedf3abe9fa7148164f216c252a0a902
mardiniii <sezama03@hotmail.com>
2018-03-28 13:31:55 -0600

    Update Queue with getters and setters

--- queue/node.go
+++ queue/node.go
@@ -2,7 +2,41 @@ package main
 
 // Node struct
 type Node struct {
-	Value    interface{}
-	Previous *Node
-	Next     *Node
+	value    interface{}
+	previous *Node
+	next     *Node
+}
+
+// Getters
+
+// Next method to return the next node if exists
+func (n *Node) Next() *Node {
+	return n.next
+}
+
+// Previous method to return the previous node if exists
+func (n *Node) Previous() *Node {
+	return n.previous
+}
+
+// Value method to return the current node value
+func (n *Node) Value() interface{} {
+	return n.value
+}
+
+// Setters
+
+// SetNext method to set the next node
+func (n *Node) SetNext(next *Node) {
+	n.next = next
+}
+
+// SetPrevious method to set the previous node
+func (n *Node) SetPrevious(previous *Node) {
+	n.previous = previous
+}
+
+// SetValue method to set the value
+func (n *Node) SetValue(val interface{}) {
+	n.value = val
 }
--- queue/queue.go
+++ queue/queue.go
@@ -4,54 +4,90 @@ import "fmt"
 
 // Queue struct
 type Queue struct {
-	End    *Node
-	Front  *Node
-	Length int
+	front  *Node
+	end    *Node
+	length int
 }
+
+// Getters
+
+// Front method to return the current front node
+func (q *Queue) Front() *Node {
+	return q.front
+}
+
+// End method to return the current end node
+func (q *Queue) End() *Node {
+	return q.end
+}
+
+// Length method to return the current length
+func (q *Queue) Length() int {
+	return q.length
+}
+
+// Setters
+
+// SetFront method to set the front node
+func (q *Queue) SetFront(n *Node) {
+	q.front = n
+}
+
+// SetEnd method to set the end node
+func (q *Queue) SetEnd(n *Node) {
+	q.end = n
+}
+
+// SetLength method to set the length
+func (q *Queue) SetLength(l int) {
+	q.length = l
+}
+
+// Queue structure methods
 
 // Enqueue method to place a value into the end of the queue
 func (q *Queue) Enqueue(val interface{}) {
-	newNode := &Node{Value: val}
+	newNode := &Node{value: val}
 
-	if q.Length == 0 {
-		q.Front = newNode
-		q.End = newNode
+	if q.Length() == 0 {
+		q.SetFront(newNode)
+		q.SetEnd(newNode)
 	} else {
-		currentEnd := q.End
-		currentEnd.Previous = newNode
+		currentEnd := q.End()
+		currentEnd.SetPrevious(newNode)
 
-		newNode.Next = currentEnd
-		q.End = newNode
+		newNode.SetNext(currentEnd)
+		q.SetEnd(newNode)
 	}
-	q.Length++
+	q.SetLength(q.Length() + 1)
 }
 
 // Dequeue method to remove a value into the front of the queue
 func (q *Queue) Dequeue() interface{} {
 	switch {
-	case q.Length == 0:
+	case q.Length() == 0:
 		return nil
-	case q.Length == 1:
-		currentFront := q.Front
-		q.Front = nil
-		q.End = nil
-		q.Length--
-		return currentFront.Value
+	case q.Length() == 1:
+		currentFront := q.Front()
+		q.SetFront(nil)
+		q.SetEnd(nil)
+		q.SetLength(q.Length() - 1)
+		return currentFront.Value()
 	default:
-		currentFront := q.Front
-		q.Front = currentFront.Previous
-		q.Front.Next = nil
-		currentFront.Previous = nil
-		q.Length--
+		currentFront := q.Front()
+		q.SetFront(currentFront.Previous())
+		q.Front().SetNext(nil)
+		currentFront.SetPrevious(nil)
+		q.SetLength(q.Length() - 1)
 
-		return currentFront.Value
+		return currentFront.Value()
 	}
 }
 
 // Print method to print the content of the current Queue
 func (q *Queue) Print() {
-	for n := q.Front; n != nil; n = n.Previous {
-		fmt.Printf("%+v ", n.Value)
+	for n := q.Front(); n != nil; n = n.Previous() {
+		fmt.Printf("%+v ", n.Value())
 	}
 	fmt.Println()
 }
