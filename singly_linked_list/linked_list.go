2a4452d598b91daaea10bb4588f32d19139bca80
mardiniii <sezama03@hotmail.com>
2018-03-28 13:59:54 -0600

    Update SinglyLinkedList with getters and setters

--- singly_linked_list/linked_list.go
+++ singly_linked_list/linked_list.go
@@ -12,100 +12,133 @@ import "fmt"
 
 // LinkedList struct
 type LinkedList struct {
-	Head   *Node
-	Tail   *Node
-	Length int
+	head   *Node
+	tail   *Node
+	length int
 }
 
-// Print LinkedList
-func (l *LinkedList) Print() {
-	currentNode := l.Head
+// Getters
 
-	if currentNode != nil {
-		fmt.Printf("List elements: ")
-		for currentNode != nil {
-			fmt.Printf("%+v ", currentNode.Value)
-			currentNode = currentNode.Next
-		}
-		fmt.Println()
-	}
+// Head method to return the head node
+func (l *LinkedList) Head() *Node {
+	return l.head
 }
+
+// Tail method to return the tail node
+func (l *LinkedList) Tail() *Node {
+	return l.tail
+}
+
+// Length method to return the list length
+func (l *LinkedList) Length() int {
+	return l.length
+}
+
+// Setters
+
+// SetHead method to set the head node
+func (l *LinkedList) SetHead(n *Node) {
+	l.head = n
+}
+
+// SetTail method to set the head node
+func (l *LinkedList) SetTail(n *Node) {
+	l.tail = n
+}
+
+// SetLength method to set the head node
+func (l *LinkedList) SetLength(length int) {
+	l.length = length
+}
+
+// Linked list structure methods
 
 // Insert new node at the end of the linked list
 func (l *LinkedList) Insert(val interface{}) {
-	n := &Node{Value: val}
+	n := &Node{value: val}
 
-	if l.Head == nil {
-		l.Head = n
+	if l.Head() == nil {
+		l.SetHead(n)
 	} else {
-		l.Tail.Next = n
+		l.Tail().SetNext(n)
 	}
-	l.Tail = n
-	l.Length++
+	l.SetTail(n)
+	l.SetLength(l.Length() + 1)
 }
 
 // InsertAt method adds a new value at the given position
 func (l *LinkedList) InsertAt(pos int, val interface{}) {
-	n := &Node{Value: val}
+	n := &Node{value: val}
 	// If the given position is lower than the list length
 	// the element will be inserted at the end of the list
 	switch {
-	case l.Length < pos:
+	case l.Length() < pos:
 		l.Insert(val)
 	case pos == 1:
-		n.Next = l.Head
-		l.Head = n
+		n.SetNext(l.Head())
+		l.SetHead(n)
 	default:
-		currentNode := l.Head
+		node := l.Head()
 		// Position - 2 since we want the element replacing the given position
 		for i := 1; i < (pos - 1); i++ {
-			currentNode = currentNode.Next
+			node = node.Next()
 		}
-		n.Next = currentNode.Next
-		currentNode.Next = n
+		n.SetNext(node.Next())
+		node.SetNext(n)
 	}
 
-	l.Length++
+	l.SetLength(l.Length() + 1)
 }
 
 // Get value in the given position
 func (l *LinkedList) Get(pos int) interface{} {
-	if pos > l.Length {
+	if pos > l.Length() {
 		return nil
 	}
 
-	currentNode := l.Head
+	node := l.Head()
 	// Position - 1 since we want the value in the given position
 	for i := 0; i < pos-1; i++ {
-		currentNode = currentNode.Next
+		node = node.Next()
 	}
 
-	return currentNode.Value
+	return node.Value()
 }
 
 // Delete value at the given position
 func (l *LinkedList) Delete(pos int) bool {
-	if pos > l.Length {
+	if pos > l.Length() {
 		return false
 	}
 
-	currentNode := l.Head
+	node := l.Head()
 	if pos == 1 {
-		l.Head = currentNode.Next
+		l.SetHead(node.Next())
 	} else {
 		for i := 1; i < pos-1; i++ {
-			currentNode = currentNode.Next
+			node = node.Next()
 		}
-		currentNode.Next = currentNode.Next.Next
+		node.SetNext(node.Next().Next())
 	}
-	l.Length--
+	l.SetLength(l.Length() - 1)
 	return true
 }
 
 // Each method to apply to each element in the linked list a
 // function who receives an interface and don't return any value
 func (l *LinkedList) Each(f func(val interface{})) {
-	for n := l.Head; n != nil; n = n.Next {
-		f(n.Value)
+	for n := l.Head(); n != nil; n = n.Next() {
+		f(n.Value())
+	}
+}
+
+// Print LinkedList
+func (l *LinkedList) Print() {
+	if l.Head() != nil {
+		fmt.Printf("List elements: ")
+		for node := l.Head(); node != nil; node = node.Next() {
+			fmt.Printf("%+v ", node.Value())
+		}
+		fmt.Println()
 	}
 }
--- singly_linked_list/node.go
+++ singly_linked_list/node.go
@@ -2,12 +2,30 @@ package main
 
 // Node struct
 type Node struct {
-	Value interface{}
-	Next  *Node
+	value interface{}
+	next  *Node
 }
 
-// NextNode function to return the next node if exists
-// As an example of how to create a structure method
-func (n *Node) NextNode() *Node {
-	return n.Next
+// Getters
+
+// Value method to return the current node value
+func (n *Node) Value() interface{} {
+	return n.value
+}
+
+// Next method to return the next node if exists
+func (n *Node) Next() *Node {
+	return n.next
+}
+
+// Setters
+
+// SetNext method to set the next node
+func (n *Node) SetNext(next *Node) {
+	n.next = next
+}
+
+// SetValue method to set the value
+func (n *Node) SetValue(val interface{}) {
+	n.value = val
 }
